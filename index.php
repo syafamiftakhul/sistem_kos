d09a73792034721584d39c5f4dde16f790d187d8
auliakhanzant <auliakhanzant@gmail.com>
2026-05-11 10:58:29 +0700

    ubah

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
   <meta name="viewport" content="initial-scale=1, width=device-width" />
 
   <link rel="stylesheet" href="assets/css/global.css" />
-  <link rel="stylesheet" href="assets/css/index.css" />
+  <link rel="stylesheet" href="assets/css/index.css?v=2" />
 
   <link
     rel="stylesheet"
